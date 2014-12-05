9e9bc4d39c2599284c77a2dbbeb6a098062b434b
Alex Hartwell <hartwellalex@gmail.com>
2014-12-05 13:03:51 -0500

    asdfas

--- server.js
+++ server.js
@@ -140,8 +140,8 @@ app.get('/route/:endPoint', function(req, res){
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8081
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-server.listen(server_port, server_ip_address, function(){
-  console.log("Listening on " + server_ip_address + ", server_port " + port)
+app.listen(server_port, server_ip_address, function(){
+  console.log("Listening on " + server_ip_address + ", server_port " + server_port)
 });
 
 console.log('Magic happens on port 8081');
